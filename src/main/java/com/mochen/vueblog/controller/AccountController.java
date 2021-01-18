b720e6918d4a54ab0b93185285baddd5c4b7c185
mochen <442535701@qq.com>
2021-01-18 12:32:45 +0800

    后端完成

--- src/main/java/com/mochen/vueblog/controller/AccountController.java
+++ src/main/java/com/mochen/vueblog/controller/AccountController.java
@@ -11,6 +11,7 @@ import com.mochen.vueblog.entity.User;
 import com.mochen.vueblog.service.UserService;
 import com.mochen.vueblog.util.JwtUtils;
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -21,40 +22,41 @@ import java.sql.ResultSet;
 
 @RestController
 public class AccountController {
-
-    @Autowired
-    UserService userService;
     @Autowired
     JwtUtils jwtUtils;
-
+    @Autowired
+    UserService userService;
+    /**
+     * 默认账号密码：mochen / 111111
+     *
+     */
     @CrossOrigin
     @PostMapping("/login")
     public Result login(@Validated @RequestBody LoginDto loginDto, HttpServletResponse response) {
-
         User user = userService.getOne(new QueryWrapper<User>().eq("username", loginDto.getUsername()));
         Assert.notNull(user, "用户不存在");
-        if (user.getPassword().equals(SecureUtil.md5(loginDto.getPassword()))) {
-            return Result.fail("密码错误");
+        if(!user.getPassword().equals(loginDto.getPassword())) {
+            return Result.fail("密码错误！");
         }
         String jwt = jwtUtils.generateToken(user.getId());
         response.setHeader("Authorization", jwt);
         response.setHeader("Access-Control-Expose-Headers", "Authorization");
-
-
+        // 用户可以另一个接口
         return Result.succ(MapUtil.builder()
                 .put("id", user.getId())
                 .put("username", user.getUsername())
-                .put("avater", user.getAvatar())
+                .put("avatar", user.getAvatar())
                 .put("email", user.getEmail())
                 .map()
         );
     }
 
-
-    @PostMapping("/logout")
+    // 退出
+    @GetMapping("/logout")
+    @RequiresAuthentication
     public Result logout() {
         SecurityUtils.getSubject().logout();
         return Result.succ(null);
     }
-
 }
+
